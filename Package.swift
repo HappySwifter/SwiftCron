94a43bbb73f3c05bfbb3f6bc331a432c2efeb3ff
Artem Valiev <artem.valiev@gmail.com>
2022-01-27 14:32:37 +0300

    spm fix

--- Package.swift
+++ Package.swift
@@ -1,7 +1,8 @@
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "SwiftCron",
-    targets: [],
-    dependencies:[]
+    dependencies:[],
+    targets: []
 )
